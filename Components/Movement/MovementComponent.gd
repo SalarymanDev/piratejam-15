90af2c49d462c229e63ae7f96e7e456494d8dac5
Robert Kennedy <robert076kennedy@gmail.com>
2024-07-18 18:39:13 -0400

    Setup project structure & enforced static typing

---
+++ Components/Movement/MovementComponent.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name MovementComponent
+
+@export var character: CharacterBody2D
+@export var speed: float
---
+++ Components/Navigation/NavigationComponent.gd
@@ -0,0 +1,5 @@
+extends NavigationAgent2D
+class_name NavigationComponent
+
+@export var movement_component: MovementComponent
+
