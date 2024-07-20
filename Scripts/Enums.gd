9ddb6f1f3d6d32a656300c9d111c8fe68d39397e
Henry Haner <clarkstraws@gmail.com>
2024-07-20 19:18:06 -0400

    initial push with resource

---
+++ Scripts/Enums.gd
@@ -0,0 +1,8 @@
+class_name Enums
+
+enum Ingredients {
+	MandrakeRoot = 1,
+	Wolfsbane = 2,
+	Salt = 4
+}
+
---
+++ Scripts/IngredientResource.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name IngredientResource
+
+
+@export var ingredient_value: Enums.Ingredients
+@export var name: String
+@export var description: String
+@export var image: Image
+
+
