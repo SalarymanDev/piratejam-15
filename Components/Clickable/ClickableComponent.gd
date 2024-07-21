07f07c6b1d7e8f70bcdce54d18fb383cf39b6536
Robert Kennedy <robert076kennedy@gmail.com>
2024-07-21 01:17:14 -0400

    Implemented Tooltips & Added Sprites for Water/Wood

--- Components/Clickable/ClickableComponent.gd
+++ Components/Clickable/ClickableComponent.gd
@@ -1,7 +1,20 @@
 extends Area2D
+class_name ClickableComponent
+
+@export var tooltip_text: String
+
+@onready var label: Label = $Label
+
+func _ready() -> void:
+	label.text = tooltip_text
+
+func update_tooltip(text: String) -> void:
+	label.text = text
 
 func _on_mouse_entered() -> void:
+	label.visible = true
 	MouseManager.set_target(get_parent() as Node2D)
 
 func _on_mouse_exited() -> void:
+	label.visible = false
 	MouseManager.set_target(null)
--- Scenes/Equipment/IngredientSpawner/IngredientSpawner.gd
+++ Scenes/Equipment/IngredientSpawner/IngredientSpawner.gd
@@ -4,18 +4,21 @@ extends StaticBody2D
 @export var ingredient: IngredientResource
 @export var texture: Texture2D
 @export var audio_clips: Array[AudioStream]
+@export var tooltip: String
 
 @onready var item_component: ItemComponent = $ItemComponent
 @onready var audio_component: AudioComponent = $AudioComponent
+@onready var clickable_component: ClickableComponent = $ClickableComponent
 @onready var sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-	if ingredient:
-		item_component.item = ingredient
+	assert(ingredient)
+	item_component.item = ingredient
 	if texture:
 		sprite.texture = texture
 	if audio_clips:
 		audio_component.audio_clips = audio_clips
+	clickable_component.update_tooltip(tooltip)
 
 func _process(_delta: float) -> void:
 	if not Engine.is_editor_hint():
