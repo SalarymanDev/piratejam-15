47377b57c52fafc4181a965186767c0284b2f8b6
Henry Haner <clarkstraws@gmail.com>
2024-07-23 23:07:23 -0400

    additional table asset stuff

---
+++ Components/StoreItem/StoreItemComponent.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name StoreItemComponent
+
+@export var disabled: bool = false
+@export var _has_item: bool = false	
+@export var _stored_item: ItemResource
+
+@onready var item_component: ItemComponent = get_parent().find_child("ItemComponent")
+
+##signal store_item_event(item: ItemResource)
+
+func store(item: ItemResource) -> void:
+	_stored_item = item
+	item_component.item = item
+	_has_item = true
+	print("Currently storing a " + _stored_item.name)
+
+func dispense() -> void:
+	_has_item = !_has_item
+	_stored_item = null
---
+++ Scenes/Equipment/Table/Table.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+@onready var audio_component: AudioComponent = $AudioComponent
+@onready var store_item_component: StoreItemComponent = $StoreItemComponent
+@onready var stored_item_sprite: Sprite2D = $StoredItemSprite
+
+func _on_drop_off_ingredient_event(_ingredient: IngredientResource) -> void:
+	audio_component.play()
+	print("dropped off " + _ingredient.name)
+	stored_item_sprite.texture = _ingredient.texture
+	store_item_component.store(_ingredient as IngredientResource)
+	
+
+func _on_drop_off_potion_event(_potion: PotionResource) -> void:
+	audio_component.play()
+	print("dropped off " + _potion.name)
+	stored_item_sprite.texture = _potion.texture
+	store_item_component.store(_potion as PotionResource)
+
+
+func _on_picked_up_event() -> void:
+	audio_component.play()
+	print("picking up")
+	stored_item_sprite.texture = null
+	store_item_component.dispense()
+	
