9596abe90bc78d7265ad2305259ba4cafbd854f2
Robert Kennedy <robert076kennedy@gmail.com>
2024-07-23 20:08:01 -0400

    Updated potion button arrow based on whether it's enabled

--- Scenes/UI/MainUI/MainUI.gd
+++ Scenes/UI/MainUI/MainUI.gd
@@ -6,6 +6,7 @@ extends Control
 @onready var money_label: Label = $MoneyPaper/HBoxContainer/MoneyLabel
 @onready var day_label: Label = $DayPaper/DayLabel
 @onready var invisbility_potion_texture: TextureRect = $InvisibilityButton/InvisibilityPotionTexture
+@onready var invsibility_potion_button: TextureButton = $InvisibilityButton
 
 var _current_time_index: int = 0
 var _has_invisiblity_potion: bool = false
@@ -33,6 +34,10 @@ func _update_day(current_day: int) -> void:
 	day_label.text = "Day %d" % current_day
 
 func _update_potion() -> void:
+	if _has_invisiblity_potion:
+		invsibility_potion_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+	else:
+		invsibility_potion_button.mouse_default_cursor_shape = Control.CURSOR_ARROW
 	invisbility_potion_texture.visible = _has_invisiblity_potion
 
 func _on_invisibility_button_pressed() -> void:
