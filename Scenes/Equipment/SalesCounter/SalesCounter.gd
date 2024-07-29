feb573ccbb4a8728ae7f44731745956611fb3ae8
Ryan Scott <r.l.scott2014@gmail.com>
2024-07-29 18:47:05 -0400

    Sprite stuff

--- Scenes/Equipment/SalesCounter/SalesCounter.gd
+++ Scenes/Equipment/SalesCounter/SalesCounter.gd
@@ -6,12 +6,23 @@ extends StaticBody2D
 @onready var audio_component: AudioComponent = $AudioComponent
 @onready var potion_sold_spawn_point: Node2D = $PotionSoldSpawnPoint
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var clickable_component: ClickableComponent = $ClickableComponent
+@onready var dropoff_component: DropOffComponent = $DropOffComponent
 
 func _ready() -> void:
+	var scalar := Vector2(instance_scale, instance_scale)
 	if Engine.is_editor_hint():
-		sprite.apply_scale(Vector2(instance_scale, instance_scale))
+		sprite.apply_scale(scalar)
+		collision.apply_scale(scalar)
+		clickable_component.apply_scale(scalar)
+		dropoff_component.apply_scale(scalar)
 		return
-	sprite.apply_scale(Vector2(instance_scale, instance_scale))
+	
+	sprite.apply_scale(scalar)
+	collision.apply_scale(scalar)
+	clickable_component.apply_scale(scalar)
+	dropoff_component.apply_scale(scalar)
 	
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
