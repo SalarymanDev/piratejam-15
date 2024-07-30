b346b444e9bd4fd90cd5be3df40c14e01e95adac
Robert Kennedy <robert076kennedy@gmail.com>
2024-07-30 17:58:15 -0400

    Refactored tooltips to follow cursor

--- Components/Clickable/ClickableComponent.gd
+++ Components/Clickable/ClickableComponent.gd
@@ -3,19 +3,16 @@ class_name ClickableComponent
 
 @export var tooltip_text: String
 
-@onready var label: Label = $Label
-
-func _ready() -> void:
-	label.visible = false
-	label.text = tooltip_text
+@onready var tooltip: String = tooltip_text
 
 func update_tooltip(text: String) -> void:
-	label.text = text
+	tooltip = text
 
 func _on_mouse_entered() -> void:
-	label.visible = true
+	MouseManager.update_tooltip(tooltip)
+	MouseManager.show_tooltip(true)
 	MouseManager.set_target(get_parent() as Node2D)
 
 func _on_mouse_exited() -> void:
-	label.visible = false
 	MouseManager.set_target(null)
+	MouseManager.show_tooltip(false)
--- Scripts/Managers/MouseManager.gd
+++ Scripts/Managers/MouseManager.gd
@@ -4,8 +4,10 @@ var tooltip_label: Label = Label.new()
 var _current_target: Node2D
 
 func _ready() -> void:
-	tooltip_label.visible = true
-	tooltip_label.text = "Test"
+	tooltip_label.visible = false
+	tooltip_label.z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+	tooltip_label.theme = preload("res://Assets/main.tres")
+	tooltip_label.add_theme_font_size_override("font_size", 24)
 	get_tree().root.add_child.call_deferred(tooltip_label)
 
 func set_target(new_target: Node2D) -> void:
@@ -20,8 +22,8 @@ func has_target() -> bool:
 func show_tooltip(show: bool) -> void:
 	tooltip_label.visible = show
 
-func update_tooltip() -> void:
-	pass
+func update_tooltip(text: String) -> void:
+	tooltip_label.text = text
 
 func _process(_delta: float) -> void:
 	tooltip_label.global_position = get_global_mouse_position() + Vector2(30, 30)
