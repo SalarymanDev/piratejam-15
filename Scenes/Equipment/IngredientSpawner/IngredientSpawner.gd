493080b071abaf87ec92af2501b23db23bf1c2c5
Ryan Scott <rscott@srspharmacy.com>
2024-07-30 11:26:22 -0400

    Add item emblem sprite to spawners

--- Scenes/Equipment/IngredientSpawner/IngredientSpawner.gd
+++ Scenes/Equipment/IngredientSpawner/IngredientSpawner.gd
@@ -5,22 +5,28 @@ extends StaticBody2D
 @export var texture: Texture2D
 @export var audio_clips: Array[AudioStream]
 @export var tooltip: String
+@export var shouldDisplayEmblem: bool = true
 @export var instance_scale: float = 1.0
 
 @onready var item_component: ItemComponent = $ItemComponent
 @onready var audio_component: AudioComponent = $AudioComponent
 @onready var clickable_component: ClickableComponent = $ClickableComponent
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $SpawnerSprite
+@onready var emblem: Sprite2D = $ItemEmblemSprite
 
 func _ready() -> void:
 	assert(texture)
 	if Engine.is_editor_hint():
 		sprite.texture = texture
+		if shouldDisplayEmblem:
+			emblem.texture = item_component.item.texture
 		sprite.apply_scale(Vector2(instance_scale, instance_scale))
 		return
 	assert(ingredient)
 	item_component.item = ingredient
 	sprite.texture = texture
+	if shouldDisplayEmblem:
+		emblem.texture = item_component.item.texture
 	if audio_clips:
 		audio_component.audio_clips = audio_clips
 	if clickable_component:
@@ -32,6 +38,8 @@ func _process(_delta: float) -> void:
 		return
 	if sprite.texture != texture:
 		sprite.texture = texture
+	if emblem.texture != item_component.item.texture and shouldDisplayEmblem:
+		emblem.texture = item_component.item.texture
 
 func _on_picked_up_event() -> void:
 	audio_component.play()
